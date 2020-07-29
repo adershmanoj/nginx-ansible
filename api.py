271b20e27539628ac88475be0bddbc9bb25ee255
adershmanoj <adershmanoj@gmail.com>
2020-07-29 23:33:47 +0530

    Flask server with 3 endpoints

---
+++ api.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+PORT = 5000
+
+app = Flask(__name__)
+
+def response(text: str):
+    return text, 200, {'Content-Type': 'text/plain; charset=utf-8'}
+
+@app.route('/internal')
+def internal():
+    return response("internal")
+
+@app.route('/external')
+def external():
+    return response("external")
+
+@app.route('/cached')
+def cached():
+    return response("cached")
+
+if __name__ == '__main__':
+    app.run(port=PORT)
